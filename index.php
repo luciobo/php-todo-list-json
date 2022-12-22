6ebec59993134b626343aa8f79e0cc388b4576c4
luciobo <luciobo@icloud.com>
2022-12-22 16:17:19 +0100

    cambiato colore cestino

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
                      <ul class="list-group list-group-flush">
                         <li class="list-group-item d-flex justify-content-between">
                            <span>ciao</span>
-                           <i class="fa-regular fa-trash-can p-1"></i>
+                           <a class="text-danger" href=""><i class="fa-regular fa-trash-can p-1"></i></a>
                         </li>
                      </ul>
                   </div>
